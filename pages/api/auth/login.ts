de2dacc903ef89367199b3415ce4233343aa849f
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2024-03-06 13:51:43 -0500

    upgrade to nextjs v13

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -5,9 +5,29 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const { name, message } = req.body
+ 
   try {
-    /*const { email, password } = req.body
-    await signIn('credentials', { email, password })*/
+    // Redirect client to url if form successfully submitted
+    //await handleFormInputAsync({ name, message })
+    const AUTH_TYPE = 'code'
+    const CLIENT_ID = 'dashboard'
+    res.redirect(307, `https://id.purduehackers.com/api/authorize`
+        + `?response_type=${AUTH_TYPE}&client_id=${CLIENT_ID}`)
+
+    
+  } catch (err) {
+    res.status(500).send({ error: 'Failed to fetch data' })
+  }
+}
+
+/*export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    //const { email, password } = req.body
+    //await signIn('credentials', { email, password })
  
     res.status(200).json({ success: true })
   } catch (error) {
@@ -17,19 +37,4 @@ export default async function handler(
       res.status(500).json({ error: 'Something went wrong.' })
     }
   }
-}
- 
-export async function handler2(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { name, message } = req.body
- 
-  try {
-    // Redirect client to url if form successfully submitted
-    //await handleFormInputAsync({ name, message })
-    res.redirect(307, '/')
-  } catch (err) {
-    res.status(500).send({ error: 'Failed to fetch data' })
-  }
-}
+}*/
